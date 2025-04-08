f4e053f2bcca31fc468fef8c2145300ac44f58bd
Angelika Brown <angelikpbrown@gmail.com>
2025-04-08 10:56:00 -0400

    working chat

--- forum/models.py
+++ forum/models.py
@@ -18,9 +18,7 @@ class User(UserMixin, db.Model):
     admin = db.Column(db.Boolean, default=False)
     posts = db.relationship("Post", backref="user")
     comments = db.relationship("Comment", backref="user")
-    messages = db.relationship("Message", backref="user")
-    #messages_sent = db.relationship("Message", backref='user')
-    #messages_received = db.relationship("Message", backref='user')
+
 
     def __init__(self, email, username, password):
         self.email = email
@@ -124,46 +122,6 @@ class Comment(db.Model):
             self.savedresponce =  "Just a moment ago!"
         return self.savedresponce
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text)
-    postdate = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    #composite key made of key in User model
-    # __table_args__ = (
-    #     db.PrimaryKeyConstraint(
-    #         sender_id, recipient_id,
-    #     ),
-    # )
-
-    lastcheck = None
-    savedresponce = None
-
-    def __init__(self,  content, postdate):
-        self.content = content
-        self.postdate = postdate
-
-    def get_time_string(self):
-        # this only needs to be calculated every so often, not for every request
-        # this can be a rudamentary chache
-        now = datetime.datetime.now()
-        if self.lastcheck is None or (now - self.lastcheck).total_seconds() > 30:
-            self.lastcheck = now
-        else:
-            return self.savedresponce
-
-        diff = now - self.postdate
-        seconds = diff.total_seconds()
-        print(seconds)
-        if seconds / (60 * 60 * 24 * 30) > 1:
-            self.savedresponce = " " + str(int(seconds / (60 * 60 * 24 * 30))) + " months ago"
-        elif seconds / (60 * 60 * 24) > 1:
-            self.savedresponce = " " + str(int(seconds / (60 * 60 * 24))) + " days ago"
-        else:    
-            self.savedresponce = "Just a moment ago!"
-
-        return self.savedresponce
 
 class Reply(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -206,26 +164,26 @@ class Reply(db.Model):
         return self.savedresponce
 
 def error(errormessage):
-	return "<b style=\"color: red;\">" + errormessage + "</b>"
+    return "<b style=\"color: red;\">" + errormessage + "</b>"
 
 def generateLinkPath(subforumid):
-	links = []
-	subforum = Subforum.query.filter(Subforum.id == subforumid).first()
-	parent = Subforum.query.filter(Subforum.id == subforum.parent_id).first()
-	links.append("<a href=\"/subforum?sub=" + str(subforum.id) + "\">" + subforum.title + "</a>")
-	while parent is not None:
-		links.append("<a href=\"/subforum?sub=" + str(parent.id) + "\">" + parent.title + "</a>")
-		parent = Subforum.query.filter(Subforum.id == parent.parent_id).first()
-	links.append("<a href=\"/\">Forum Index</a>")
-	link = ""
-	for l in reversed(links):
-		link = link + " / " + l
-	return link
+    links = []
+    subforum = Subforum.query.filter(Subforum.id == subforumid).first()
+    parent = Subforum.query.filter(Subforum.id == subforum.parent_id).first()
+    links.append("<a href=\"/subforum?sub=" + str(subforum.id) + "\">" + subforum.title + "</a>")
+    while parent is not None:
+        links.append("<a href=\"/subforum?sub=" + str(parent.id) + "\">" + parent.title + "</a>")
+        parent = Subforum.query.filter(Subforum.id == parent.parent_id).first()
+    links.append("<a href=\"/\">Forum Index</a>")
+    link = ""
+    for l in reversed(links):
+        link = link + " / " + l
+    return link
 
 
 #Post checks
 def valid_title(title):
-	return len(title) > 4 and len(title) < 140
+    return len(title) > 4 and len(title) < 140
 def valid_content(content):
-	return len(content) > 10 and len(content) < 5000
+    return len(content) > 10 and len(content) < 5000
 
